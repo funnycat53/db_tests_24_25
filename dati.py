987f09e00f7317e6eb5798c850b7b27163055d60
funnycat53 <patriks.jkb@gmail.com>
2024-10-31 16:38:52 +0200

    real

--- dati.py
+++ dati.py
@@ -3,7 +3,7 @@ import sqlite3
 
 conn = sqlite3.connect("dati.db", check_same_thread=False)
 
-def tabulas_izveide():
+def skolenu_tabulas_izveide():
     cur = conn.cursor()
     cur.execute(
         """
@@ -19,6 +19,7 @@ def tabulas_izveide():
 # tabulas_izveide()
 
 def pievienot_skolenu(vards, uzvards):
+    print(vards, uzvards)
     cur = conn.cursor()
     cur.execute(
         f"""
@@ -26,3 +27,50 @@ def pievienot_skolenu(vards, uzvards):
         """
     )
     conn.commit()
+
+def skolotaju_tabulas_izveide():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        CREATE TABLE skolotaji(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        vards TEXT NOT NULL,
+        uzvards TEXT NOT NULL
+        )
+        """
+    )
+    conn.commit()
+
+def pievienot_skolotaju(vards, uzvards):
+    cur = conn.cursor()
+    cur.execute(
+        f"""
+        INSERT INTO skolotaji(vards, uzvards) VALUES("{vards}","{uzvards}")
+        """
+    )
+    conn.commit()
+
+    print(vards, uzvards)
+
+def pievienot_prieksmetu(prieksmets):
+    print(prieksmets)
+
+def iegut_skolenus():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT vards, uzvards FROM skoleni"""
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def iegut_skolotajus():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT vards, uzvards, id FROM skolotaji"""
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+
--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import sqlite3
 from flask import Flask, render_template, request
-from dati import pievienot_skolenu
+from dati import pievienot_skolenu, pievienot_skolotaju, pievienot_prieksmetu, iegut_skolenus, iegut_skolotajus
 
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST","GET"])
 def index():
+    skoleni_no_db = iegut_skolenus()
+    skolotaji_no_db = iegut_skolotajus()
+
     if request.method == "POST":
         vards = request.form['name']
         uzvards = request.form['lastname']
-        pievienot_skolenu(vards, uzvards)
-        dati = vards+" "+uzvards
+        skolotajs_vards = request.form['sk_name']
+        skolotajs_uzvards = request.form['sk_lastname']
+        prieksmets = request.form['prieksmets']
+        if vards and uzvards:
+            pievienot_skolenu(vards, uzvards)
+        if skolotajs_vards and skolotajs_uzvards:
+            pievienot_skolotaju(skolotajs_vards, skolotajs_uzvards)
+        if prieksmets:
+            pievienot_prieksmetu(prieksmets)
 
-        return render_template("index.html", aizsutitais = dati)
+        dati = f"Pievienots skolēns - {vards} {uzvards}, skolotājs - {skolotajs_vards} {skolotajs_uzvards}, mācību priekšmets - {prieksmets}"
+
+        return render_template("index.html", aizsutitais = dati, skoleni = skoleni_no_db, skolotaji = skolotaji_no_db)
     
     # Get metode
-    return render_template("index.html")
+    return render_template("index.html", skoleni = skoleni_no_db, skolotaji = skolotaji_no_db)
 
-@app.route("/pievienot")
+@app.route("/pievienot", methods=["POST", "GET"])
 def pievienot():
-    return render_template("pievienot.html")
+    skolotaji = iegut_skolotajus()
+    if request.method == "POST":
+        print(request.form["skolotajs"])
+    return render_template("pievienot.html", skolotaji = skolotaji)
 
 @app.route("/atzimes")
 def atzimes():
